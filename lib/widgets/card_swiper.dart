b5c47bd6d8e1c2a80959a09d1153232d974612b8
Rita Casiello <rituzka@gmail.com>
2022-04-10 19:54:57 +0200

    new navigator widget in card screen

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -6,6 +7,27 @@ class CardSwiper extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return Container(
-        width: double.infinity, height: size.height * 0.5, color: Colors.green);
+      width: double.infinity,
+      height: size.height * 0.5,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'detail',
+                arguments: 'movie_instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                  placeholder: AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  fit: BoxFit.cover),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
